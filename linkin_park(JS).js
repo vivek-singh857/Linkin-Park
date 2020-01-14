c48d542bd618f85837dd9e23324702430599dc5f
Vivek Kumar Singh <vivek.kumar.singh857@gmail.com>
2020-01-15 00:37:12 +0530

    Files added

---
+++ linkin_park(JS).js
@@ -0,0 +1,3 @@
+alert("Disclamer: It is just a fan page created to practice my skills if anybody is somehow hurt i apologize.")
+alert("This site is created using HTML,CSS,Bootstrap and Javascript")
+alert("Some videos might not play due to YOUTUBE embed policy.")
